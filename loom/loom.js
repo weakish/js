4d24a9b7a1d626b0050a8f3c656d8cb8e718c638
Jang Rush <weakish@gmail.com>
2019-12-21 19:44:39 +0800

    :new:loom: change verify key size from 32 to 8
    8  is enough for verification.
    This also makes encrypt key and verify key distinguishable on size.
    
    This commit also refactor with opaque types.

--- loom/loom.js
+++ loom/loom.js
@@ -10,37 +10,68 @@ const makeDir = require("make-dir");
 const prompts = require("prompts");
 const simpleGit = require("simple-git/promise");
 
-/** @type {string} */
-const repo = path.join(os.homedir(), "loom");
-/** @type {string} */
-const verifyKeyPath = path.join(repo, ".loom-verify.key");
+/**
+ * @template T, U
+ * @typedef {U & { readonly __TYPE__: T}} Opaque */
+
+/** @typedef {Opaque<'path', string>} Path */
+/** @type {function(string): Path} */
+const newPath = filePath => {
+  if (filePath.includes("\0")) {
+    throw new Error("path cannot contain NUL character");
+  } else {
+    return /** @type Path */ (filePath);
+  }
+};
+
+/** @type {Path} */
+const repo = newPath(path.join(os.homedir(), "loom"));
+/** @type {Path} */
+const verifyKeyPath = newPath(path.join(repo, ".loom-verify.key"));
 /** @type {function(): boolean} */
 const hasVerifyKey = () => hasFileOrDirectory(verifyKeyPath, "file");
-/** @type {function(): string} */
-const readVerifyKey = () => {
-  return fs.readFileSync(verifyKeyPath, "utf-8");
+
+/** @typedef {Opaque<'verifyKey', string>} VerifyKey */
+/** @type {function(string): VerifyKey} */
+const newVerifyKey = k => {
+  if (/^[0-9a-fA-F]{8}$/.test(k)) {
+    return /** @type {VerifyKey} */ (k);
+  } else {
+    throw new Error("verify key length must be a hex string of length 8");
+  }
 };
-/** @type {function(string): void} */
+/** @type {function(): VerifyKey} */
+const readVerifyKey = () => {
+  return newVerifyKey(fs.readFileSync(verifyKeyPath, "utf-8"));
+};
+/** @type {function(VerifyKey): void} */
 const saveVerifyKey = verifyKey => {
   fs.writeFileSync(verifyKeyPath, verifyKey, "utf-8");
 };
+/** @typedef {Opaque<'encryptKey', string>} EncryptKey */
+/** @type {function(string): EncryptKey} */
+const newEncryptKey = k => {
+  if (/^[0-9a-fA-F]{32}$/.test(k)) {
+    return /** @type {EncryptKey} */ (k);
+  } else {
+    throw new Error("encrypt key length must be a hex string of length 32");
+  }
+};
 
-/** @typedef {{filePath: string, needEncryption: boolean}} SourceFile */
-/** @type{function(string, boolean): SourceFile} */
+/** @typedef {{filePath: Path, needEncryption: boolean}} SourceFile */
+/** @type{function(Path, boolean): SourceFile} */
 const newSourceFile = (filePath, needEncryption) => ({
-  filePath: path.resolve(filePath),
+  filePath: newPath(path.resolve(filePath)),
   needEncryption
 });
 
 /** @typedef {{type: 'password', name: string, message: string}} PasswordPrompt */
-
 /** @type {PasswordPrompt} */
 const passphrase = {
   type: "password",
   name: "passphrase",
   message: "Type a passphrase to generate encryption key:"
 };
-
 /** @type {PasswordPrompt} */
 const repeatedPassphrase = {
   type: "password",
@@ -48,24 +79,23 @@ const repeatedPassphrase = {
   message: "Type a passphrase to generate encryption key:"
 };
 
-/** @type {function(string): [string, string]} */
+/** @type {function(string): [EncryptKey, VerifyKey]} */
 const genKeyFromPass = pass => {
   const sha256Hex = crypto
     .createHash("sha256")
     .update(pass)
     .digest("hex");
-  const encryptKey = sha256Hex.substring(0, 32);
-  const verifyKey = sha256Hex.substring(32, 64);
+  const encryptKey = newEncryptKey(sha256Hex.substring(0, 32));
+  const verifyKey = newVerifyKey(sha256Hex.substring(56, 64));
   return [encryptKey, verifyKey];
 };
 
-/** @type {function(): Promise<string>} */
+/** @type {function(): Promise<EncryptKey>} */
 const genEncryptKey = async () => {
   if (hasVerifyKey()) {
     /** @type{prompts.Answers<"passphrase">} */
     const response = await prompts(passphrase);
     const [encryptKey, verifyKey] = genKeyFromPass(response.passphrase);
-    /** @type{string} */
     const existingVerifyKey = readVerifyKey();
     if (verifyKey === existingVerifyKey) {
       return encryptKey;
@@ -89,17 +119,13 @@ const genEncryptKey = async () => {
   }
 };
 
-/** @type {function(SourceFile, string, string): string?} */
+/** @type {function(SourceFile, Path, EncryptKey): Path?} */
 const cpSourceFile = (sourceFile, destDir, key) => {
-  /** @type {string} */
   const sourcePath = sourceFile.filePath;
   if (sourcePath.startsWith(os.homedir())) {
-    /** @type {string} */
-    const relativeSourcePath = path.relative(os.homedir(), sourcePath);
-    /** @type {string} */
-    const dest = path.join(destDir, relativeSourcePath);
+    const relativeSourcePath = newPath(path.relative(os.homedir(), sourcePath));
+    const dest = newPath(path.join(destDir, relativeSourcePath));
     makeDir.sync(path.dirname(dest));
-    /** @type {boolean} */
     if (sourceFile.needEncryption) {
       const encryptor = new Encryptor({ key });
       /** @type {string} */
@@ -117,10 +143,10 @@ const cpSourceFile = (sourceFile, destDir, key) => {
   }
 };
 
-/** @type {function(string[]): string?} */
-const pickOne = candidates => {
+/** @type {function(string[]): Path?} */
+const pickOnePath = candidates => {
   if (candidates.length === 1) {
-    return candidates[0];
+    return newPath(candidates[0]);
   } else {
     return null;
   }
@@ -131,15 +157,12 @@ const firefoxLockwise = () => {
   /** @type{string} */
   const dotFirefox = path.join(os.homedir(), ".mozilla", "firefox");
   /** @type{string[]} */
-  /** @type{string[]} */
   const loginsCandidates = globby.sync(`${dotFirefox}/*.default/logins.json`); // ignore nightly etc.
   /** @type{string[]} */
   const key4Candidates = globby.sync(`${dotFirefox}/*.default/key4.db`);
 
-  /** @type {string?} */
-  const logins = pickOne(loginsCandidates);
-  /** @type {string?} */
-  const key4 = pickOne(key4Candidates);
+  const logins = pickOnePath(loginsCandidates);
+  const key4 = pickOnePath(key4Candidates);
   if (logins === null) {
     if (key4 === null) {
       return [];
@@ -155,7 +178,7 @@ const firefoxLockwise = () => {
   }
 };
 
-/** @type{function(string, 'file' | 'directory'): boolean} */
+/** @type{function(Path, 'file' | 'directory'): boolean} */
 const hasFileOrDirectory = (fileOrDirectoryPath, fileOrDirectory) => {
   if (fs.existsSync(fileOrDirectoryPath)) {
     /** @type{fs.Stats} */
@@ -184,11 +207,11 @@ const hasFileOrDirectory = (fileOrDirectoryPath, fileOrDirectory) => {
 };
 
 /** @type{function(): boolean} */
-const hasLoomRepo = () => hasFileOrDirectory(repo + "/.git", "directory");
+const hasLoomRepo = () =>
+  hasFileOrDirectory(newPath(repo + "/.git"), "directory");
 
 /** @type {<T>(things: Array<T | null>) => Array<T>} */
 const filterOutNull = things => things.filter(thing => thing !== null);
-
 
 void (async () => {
   try {
@@ -197,9 +220,7 @@ void (async () => {
     if (!hasLoomRepo()) {
       await git.init();
     }
-    /** @type {string} */
     const encryptKey = await genEncryptKey();
-    /** @type {string[]} */
     const destPaths = filterOutNull(
       firefoxLockwise().map(sourceFile =>
         cpSourceFile(sourceFile, repo, encryptKey)
